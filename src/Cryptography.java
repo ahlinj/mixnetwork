afb43596ce9f60d567479679d51dcf8728fd1c7c
Jaka <jakaahlin0@gmail.com>
2024-02-20 00:38:38 +0100

    hybrid encryption/decryption

--- src/Cryptography.java
+++ src/Cryptography.java
@@ -1,11 +1,12 @@
 import javax.crypto.*;
+import javax.crypto.spec.SecretKeySpec;
 import java.security.*;
 import java.util.*;
 
 public class Cryptography {
 
 
-    public static KeyPair generatePubPvtKey() throws NoSuchAlgorithmException {
+    public static KeyPair generateRSAKey() throws NoSuchAlgorithmException {
         SecureRandom random = new SecureRandom();
         KeyPairGenerator KPGenerator = KeyPairGenerator.getInstance("RSA");
         KPGenerator.initialize(2048, random);
@@ -14,18 +15,38 @@ public class Cryptography {
 
 
     public static String encrypt(String plainText, PublicKey publicKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.ENCRYPT_MODE, publicKey);
-        byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
-        return Base64.getEncoder().encodeToString(encryptedBytes);
+        KeyGenerator keyGen = KeyGenerator.getInstance("AES");
+        keyGen.init(256);
+        SecretKey secretKey = keyGen.generateKey();
+
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.ENCRYPT_MODE, secretKey);
+        byte[] encryptedData = aesCipher.doFinal(plainText.getBytes());
+
+        Cipher rsaCipher = Cipher.getInstance("RSA");
+        rsaCipher.init(Cipher.ENCRYPT_MODE, publicKey);
+        byte[] encryptedKey = rsaCipher.doFinal(secretKey.getEncoded());
+
+        return Base64.getEncoder().encodeToString(encryptedKey) + ":" +
+                Base64.getEncoder().encodeToString(encryptedData);
     }
 
-    public static String decrypt(String encryptedText, PrivateKey privateKey) throws Exception {
-        Cipher cipher = Cipher.getInstance("RSA");
-        cipher.init(Cipher.DECRYPT_MODE, privateKey);
-        byte[] encryptedBytes = Base64.getDecoder().decode(encryptedText);
-        byte[] decryptedBytes = cipher.doFinal(encryptedBytes);
-        return new String(decryptedBytes);
+
+    public static String decrypt(String combined, PrivateKey privateKey) throws Exception {
+        String[] parts = combined.split(":");
+        byte[] encryptedKey = Base64.getDecoder().decode(parts[0]);
+        byte[] encryptedData = Base64.getDecoder().decode(parts[1]);
+
+        Cipher rsaCipher = Cipher.getInstance("RSA");
+        rsaCipher.init(Cipher.DECRYPT_MODE, privateKey);
+        byte[] decryptedKeyBytes = rsaCipher.doFinal(encryptedKey);
+        SecretKey secretKey = new SecretKeySpec(decryptedKeyBytes, 0, decryptedKeyBytes.length, "AES");
+
+        Cipher aesCipher = Cipher.getInstance("AES");
+        aesCipher.init(Cipher.DECRYPT_MODE, secretKey);
+        byte[] decryptedData = aesCipher.doFinal(encryptedData);
+
+        return new String(decryptedData);
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -2,19 +2,19 @@ import java.security.KeyPair;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        KeyPair keypair = Cryptography.generatePubPvtKey();
-
-        System.out.println("Public Key is: " + keypair.getPublic());
-
-        System.out.println("Private Key is: " + keypair.getPrivate());
+        KeyPair keypair1RSA = Cryptography.generateRSAKey();
+        KeyPair keypair2RSA = Cryptography.generateRSAKey();
+        KeyPair keypair3RSA = Cryptography.generateRSAKey();
 
         Message mes = new Message("Hello");
-        mes.body = Cryptography.encrypt(mes.body, keypair.getPublic());
         System.out.println(mes.body);
-        mes.body = Cryptography.decrypt(mes.body,keypair.getPrivate());
-        System.out.println(mes.body);
+        System.out.println(mes.body = Cryptography.encrypt(mes.body, keypair1RSA.getPublic()));
+        System.out.println(mes.body = Cryptography.encrypt(mes.body, keypair2RSA.getPublic()));
+        System.out.println(mes.body = Cryptography.encrypt(mes.body, keypair3RSA.getPublic()));
 
-
+        System.out.println(mes.body = Cryptography.decrypt(mes.body, keypair3RSA.getPrivate()));
+        System.out.println(mes.body = Cryptography.decrypt(mes.body, keypair2RSA.getPrivate()));
+        System.out.println(mes.body = Cryptography.decrypt(mes.body, keypair1RSA.getPrivate()));
     }
 
 
