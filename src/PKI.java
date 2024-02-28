3fd6c2b672d4be180cff965ac032abb7963cead8
Jaka <jakaahlin0@gmail.com>
2024-02-28 13:34:23 +0100

    User-port hashmap

--- src/PKI.java
+++ src/PKI.java
@@ -4,8 +4,9 @@ import java.util.concurrent.ConcurrentHashMap;
 
 public class PKI {
     public static Map<String, PublicKey> PKusermap = new ConcurrentHashMap<>();
+    public static Map<String, Integer> PortUserMap = new ConcurrentHashMap<>();
 
-    public static void addUser(String username){
+    public static void addUserPK(String username){
         try{
             KeyPair kp = Cryptography.generateRSAKey();
             PKusermap.put(username, kp.getPublic());
@@ -14,10 +15,14 @@ public class PKI {
             e.printStackTrace();
         }
     }
+    public static void addUserPort(String username, int port){
+        PortUserMap.put(username, Main.port.get());
+    }
 
     public static void removeUser(String username) {
         PKusermap.remove(username);
         Main.prKey.remove();
+        PortUserMap.remove(username);
     }
 
 
